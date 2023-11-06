0a524a2478a614c96ab4419bce7601a88d9b99ed
Priyobroto Kar <priyobrotokar@gmail.com>
2023-11-06 21:02:34 +0530

    add database connection

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(
+      `${process.env.MONGODB_URI}/youtube`
+    );
+    console.log(
+      "MONGODB connection successfull running at Port : " + process.env.PORT
+    );
+  } catch (error) {
+    console.log("MONGODB database connection error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
