a35ce5e8e68d827a58da24715bb26436eb5234ee
Priyobroto Kar <priyobrotokar@gmail.com>
2023-11-11 11:24:40 +0530

    add custom api error class, express setup and async wrapper function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+import app from "./app.js";
 import connectDB from "./db/index.js";
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(error);
+      throw error;
+    });
+    app.listen(PORT, () => console.log(`Server is running at port : ${PORT}`));
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed", err);
+  });
